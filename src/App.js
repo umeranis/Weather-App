46e379ab9eaeabbb96016ae421388c7d398301e6
Umer Anis <umeranis24@gmail.com>
2021-01-25 11:57:29 +0500

    my weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+
+
+
 
 function App() {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Karachi")
+  useEffect( () => {
+    const fetchApi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=5e473d22a22d41296b3601b7e0fbac65`
+    const response = await fetch(url)
+    const resJson = await response.json();
+    setCity(resJson.main)
+    }
+    fetchApi()
+  },[search] )
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="inputdata">
+       <input 
+       className="input"
+       type="search"
+       value={search}
+       onChange={ (event) => { setSearch(event.target.value)
+          } }
+       />
+
+       
+    </div>
+
+
+    {!city ? (
+      <p className="error">No Data Found</p>
+
+    )  :(
+      <div className="info">
+      <h2 className="temp">
+      { search }
+      </h2>
+     <h1 className="locate">
+     { city.temp }°Cel
+     </h1>
+     <h3 className="why"> Min : {city.temp_min}°Cel | Max :  {city.temp_min}°Cel</h3>
+     </div> 
+    )}
+     
     </div>
   );
 }
